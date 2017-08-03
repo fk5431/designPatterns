df7bc93ecceaff636c956bf76cb7df02a7b402c9
FK <1050577129@qq.com>
2017-08-03 11:06:48 +0800

    facade

---
+++ src/main/java/com/fk5431/facade/ShapeFacade.java
@@ -0,0 +1,29 @@
+package com.fk5431.facade;
+
+/**
+ * Created by FK on 2017/8/3.
+ */
+public class ShapeFacade {
+    private Shape circle;
+    private Shape rectangle;
+    private Shape square;
+
+    public ShapeFacade() {
+        circle = new Circle();
+        rectangle = new Rectangle();
+        square = new Square();
+    }
+
+    public void drawCircle() {
+        circle.draw();
+    }
+
+    public void drawRectangle() {
+        rectangle.draw();
+        ;
+    }
+
+    public void drawSquare() {
+        square.draw();
+    }
+}
---
+++ src/main/java/com/fk5431/facade/Test.java
@@ -0,0 +1,13 @@
+package com.fk5431.facade;
+
+/**
+ * Created by FK on 2017/8/3.
+ */
+public class Test {
+    public static void main(String[] args) {
+        ShapeFacade facade = new ShapeFacade();
+        facade.drawCircle();
+        facade.drawRectangle();
+        facade.drawSquare();
+    }
+}
